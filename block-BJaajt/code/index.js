b9457fc2284a604f63887105942c29a0226ea320
sumit <sumitthapa90@gmail.com>
2021-08-05 17:10:10 +0530

    practice

--- block-BJaajt/code/index.js
+++ block-BJaajt/code/index.js
@@ -1,29 +1,13 @@
-let input = document.querySelector("input");
-let info = document.querySelector(".info");
-let userImg = document.querySelector(".info img");
-let userName = document.querySelector(".info h2");
-let userLogin = document.querySelector(".info p");
+let catsImage = document.querySelector(".cats img");
+let catsButton = document.querySelector(".cats button");
 
-function handleDisply(userInfo) {
-  userImg.src = userInfo.avatar_url;
-  userName.innerText = userInfo.name;
-  userLogin.innerText = "@" + userInfo.login;
+function handleClick() {
+  fetch(
+    `https://api.thecatapi.com/v1/images/search?limit=1&size=full`,
+    function (catInfo) {
+      catsImage.src = catInfo[0].url;
+    }
+  );
 }
 
-function handleClick(event) {
-  if (event.keyCode === 13 && input.value) {
-    console.log(input.value);
-    const userName = new XMLHttpRequest();
-    userName.open("GET", "https://api.github.com/users/prank7");
-    userName.onload = function () {
-      handleDisply(JSON.parse(userName.response)  );
-    };
-
-    userName.onerror = function () {
-      console.log("something wentt wrong..");
-    };
-    userName.send();
-  }
-}
-
-input.addEventListener("keydown", handleClick);
+catsButton.addEventListener("click", handleClick);
