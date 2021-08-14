8d38f32bf660a5f36926b2c50667cce9b52605e3
sumit <sumitthapa90@gmail.com>
2021-08-14 14:18:26 +0530

    error

--- block-BJaajw/code/index.js
+++ block-BJaajw/code/index.js
@@ -1,36 +1,25 @@
-const url = "https://api.spaceflightnewsapi.net/v3/articles?_limit=30";
+const url = "https://api.spaceflightnewsapi11.net/v3/articles?_limit=30";
 
-let elm = document.querySelector(".news");
+let newsElm = document.querySelector(".news");
 let select = document.querySelector("select");
 let main = document.querySelector(".main");
-let errorElm = document.querySelector(".error-msg");
+let errorMsg = document.querySelector(".error-msg");
 let allNews = [];
+let isLoading = false;
 
-function handleErrorMsg(msg = "Something went wrong") {
+function handleError(message = "Something went wrong") {
   main.style.display = "none";
-  errorElm.innerText = msg;
+  errorMsg.innerText = message;
 }
 
-function handleSpinner(isLoading = false) {
+function handleSpinner() {
   if (isLoading) {
-    elm.innerHTML = `<div class="donut"></div>`;
+    newsElm.innerHTML = ` <div class="donut"></div>`;
   }
 }
 
-//   <li>
-//         <img src="./morning-2243465__340.webp" alt="" />
-//         <div>
-//           <span class="source">NASA</span>
-//           <h3>
-//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-//             modi consequatur, facere natus asperiores iste.
-//           </h3>
-//           <button>READ MORE</button>
-//         </div>
-//       </li>
-
-function displayUINews(news) {
-  elm.innerHTML = "";
+function displayNews(news) {
+  newsElm.innerHTML = "";
   news.forEach((newsItem) => {
     let li = document.createElement("li");
     let img = document.createElement("img");
@@ -38,18 +27,17 @@ function displayUINews(news) {
     img.alt = newsItem.title;
     let div = document.createElement("div");
     let span = document.createElement("span");
-    span.classList.add("option");
     span.innerText = newsItem.newsSite;
-    let h3 = document.createElement("h3");
-    h3.innerText = newsItem.summary;
+    let h2 = document.createElement("h2");
+    h2.innerText = newsItem.summary;
     let a = document.createElement("a");
-    let button = document.createElement("button");
-    button.innerText = "Read More";
-    a.append(button);
     a.href = newsItem.url;
-    div.append(span, h3, a);
+    let button = document.createElement("button");
+    a.append(button);
+    button.innerText = "READ MORE";
+    div.append(span, h2, a);
     li.append(img, div);
-    elm.append(li);
+    newsElm.append(li);
   });
 }
 
@@ -61,27 +49,48 @@ function displayOptions(sources) {
     select.append(option);
   });
 }
-
 function init() {
   isLoading = true;
   handleSpinner();
   fetch(url)
-    .then((res) => res.json())
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("Responce not ok");
+      }
+    })
     .then((news) => {
       console.log(news);
       allNews = news;
-      displayUINews(news);
-      let allSources = news.map((n) => n.newsSite);
-      displayOptions(allSources);
-    });
+      displayNews(news);
+      let allSource = Array.from(new Set(news.map((n) => n.newsSite)));
+      displayOptions(allSource);
+    })
+    .catch((error) => handleError(error));
 }
 
-function handleClick(event) {
+//   <li>
+//   <img src="./morning-2243465__340.webp" alt="" />
+//   <div>
+//     <span>NASA</span>
+//     <h2>
+//       Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+//       nihil dignissimos laudantium quo explicabo perspiciatis voluptate,
+//       adipisci beatae nam cum?
+//     </h2>
+//     <button>READ MORE</button>
+//   </div>
+// </li>
+
+select.addEventListener("change", (event) => {
   let source = event.target.value;
   let filterNews = allNews.filter((news) => news.newsSite === source);
-  displayUINews(filterNews);
-}
-select.addEventListener("click", handleClick);
+  displayNews(filterNews);
+});
 
-init();
-handleErrorMsg();
+if (navigator.onLine) {
+  init();
+} else {
+  handleError("Check your connection");
+}
